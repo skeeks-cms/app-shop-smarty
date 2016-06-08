02981ab57575e6ff736ab854ceab4564c3f606b1
Semenov Alexander <semenov@skeeks.com>
2016-06-08 16:56:32 +0300

    Update db.php

--- common/config/db.php
+++ common/config/db.php
@@ -2,15 +2,15 @@
 /**
  * @author Semenov Alexander <semenov@skeeks.com>
  * @link http://skeeks.com/
- * @copyright 2010 SkeekS (�����)
+ * @copyright 2010 SkeekS (СкикС)
  * @date 18.09.2015
  */
 return
     [
         'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=localhost;dbname=jhanway_www',
-        'username' => 'jhanway',
-        'password' => '4vhWm8ntzwVJKw57',
+        'dsn' => 'mysql:host=localhost;dbname=dbname',
+        'username' => 'username',
+        'password' => 'password',
         'charset' => 'utf8',
         'enableSchemaCache' => true,
         'schemaCacheDuration' => 3600,
